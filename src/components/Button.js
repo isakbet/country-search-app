90856e8e0d5419277cad348754ea63d37f3cab68
Yoseph Berhane <yosephbet@gmail.com>
2020-06-04 17:35:24 +0200

    added button component

---
+++ src/components/Button.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.span`	const StyledButton = styled.span`;
+padding: 10 px 40 px;
+border - radius: 5 px;
+box - shadow: 0 px 2 px 6 px rgba(0, 0, 0, 0.1);
+font - weight: 600;
+background - color: $ {
+    props => props.theme.elements
+};
+color: $ {
+    props => props.theme.fg
+};
+margin - right: 16 px;
+margin - bottom: 16 px;
+display: inline - flex;
+justify - content: space - between;
+align - items: center;
+
