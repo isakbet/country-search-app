9a5c0a136ece7395e7841bdec200e5412f14c8e0
Yoseph Berhane <yosephbet@gmail.com>
2020-06-05 17:22:19 +0200

    added main element

--- src/index.js
+++ src/index.js
@@ -1,17 +1,82 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {
+  useState
+} from "react";
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
+import {
+  ThemeProvider
+} from "styled-components";
+import {
+  DarkStyles,
+  LightStyles
+} from "./styles/Themes";
+import {
+  GlobalStyle
+} from "./styles/GlobalStyle";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Header from "./components/Header/Header";
+import CountriesHome from "./pages/CountriesHome/CountriesHome";
+import CountryDetail from "./pages/CountryDetail/CountryDetail";
+import {
+  NotFound
+} from "./pages/Error/Error";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App() {
+  const [lightTheme, setTheme] = useState(true);
+
+  const toggleTheme = () => {
+    setTheme(!lightTheme);
+  };
+
+  return ( <
+    Router >
+    <
+    ThemeProvider theme = {
+      lightTheme ? LightStyles : DarkStyles
+    } >
+    <
+    React.Fragment >
+    <
+    GlobalStyle / >
+    <
+    div className = "app__wrapper" >
+    <
+    Header toggleTheme = {
+      toggleTheme
+    }
+    lightTheme = {
+      lightTheme
+    }
+    /> <
+    main >
+    <
+    Switch >
+    <
+    Route exact path = "/"
+    component = {
+      CountriesHome
+    }
+    /> <
+    Route path = "/country/:country"
+    component = {
+      CountryDetail
+    }
+    /> <
+    Route component = {
+      NotFound
+    }
+    /> <
+    /Switch> <
+    /main> <
+    /div> <
+    /React.Fragment> <
+    /ThemeProvider> <
+    /Router>
+  );
+}
+
+ReactDOM.render( < App / > , document.getElementById("root"));
