50d5d97f8ab6e3f3bd2364e73a245cb24879360a
Yoseph Berhane <yosephbet@gmail.com>
2020-06-05 17:51:51 +0200

    added error message

---
+++ src/pages/Error/Error.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+    FaGlobeAmericas
+} from "react-icons/fa";
+
+import {
+    StyledError
+} from "./Error.styles";
+
+export const NotFound = ({
+    text
+}) => {
+    return ( <
+        StyledError >
+        <
+        h3 > {
+            text || "Page not found"
+        } < /h3> <
+        FaGlobeAmericas className = "icon" / >
+        <
+        /StyledError>
+    );
+};
